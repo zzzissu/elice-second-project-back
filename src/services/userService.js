5032b5a80244b09b6b74b8bcc9306473ffb25a44
최준영 <junzero@choejun-yeong-ui-MacBookAir.local>
2024-11-20 16:18:55 +0900

    fix : 특정 상품 조회 API

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,6 @@
 import User from '../models/user.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { InternalServerError } from '../class/errorClass.js'
 import { tokenUtil } from '../utils/authUtils.js';
 const { JWT_SECRET } = process.env;
 
