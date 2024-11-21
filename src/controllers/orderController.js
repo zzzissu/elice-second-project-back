153a102b3966dac6f592dab4dd43398e9a3993bd
Hanjs <gkswltn1223@naver.com>
2024-11-21 14:11:11 +0900

    fix: order 스키마 수정 및 코드 수정

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,34 +1,14 @@
 import OrderService from '../services/orderService.js';
-import asyncHandler from '../middleware/asyncHandler.js'; // asyncHandler를 가져옵니다.
+import asyncHandler from '../utils/asyncHandler.js'; // asyncHandler를 가져옵니다.
 
 const OrderController = {
-    // 주문 생성
-    createOrder: asyncHandler(async (req, res) => {
-        const { productId } = req.body;
-        const buyerId = req.user._id; // 로그인된 사용자 ID (buyerId)
-
-        const order = await OrderService.createOrder(productId, buyerId);
-
-        res.status(201).json({
-            message: '주문이 성공적으로 생성되었습니다.',
-            orderId: order._id,
-            product: {
-                id: order.productId._id,
-                name: order.productId.name,
-                image: order.productId.image,
-                price: order.productId.price,
-            },
-            buyerId: {
-                id: order.buyerId._id,
-                name: order.buyerId.name,
-                phone: order.buyerId.phone,
-                address: `${order.buyerId.postalCode}, ${order.buyerId.basicAdd}, ${order.buyerId.detailAdd}`,
-            },
-            createdAt: order.createdAt,
-        });
+    // 결제 페이지
+    createOrder: asyncHandler(async (req, res, next) => {
+        const order = await OrderService.createOrder(req.body);
+        res.status(201).json({ message: '주문이 성공적으로 생성되었습니다.', order });
     }),
 
-    // 로그인된 사용자가 구매한 모든 주문 조회
+    // 구매내역 조회
     getAllOrdersByBuyer: asyncHandler(async (req, res) => {
         const buyerId = req.user._id; // 로그인된 사용자 ID (buyerId)
 
@@ -44,14 +24,7 @@ const OrderController = {
                     image: order.productId.image,
                     price: order.productId.price,
                 },
-                buyerId: {
-                    id: order.buyerId._id,
-                    name: order.buyerId.name,
-                    phone: order.buyerId.phone,
-                    address: `${order.buyerId.basicAdd}, ${order.buyerId.detailAdd}`,
-                },
                 sellerId: order.sellerId,
-                soldOut: order.soldOut,
                 createdAt: order.createdAt,
             })),
         });
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,35 +1,40 @@
 import productService from '../services/productService.js';
+import asyncHandler from '../utils/asyncHandler.js';
 
 const productController = {
     // 상품 목록 조회
-    getProductList: async (req, res, next) => {
+    getProductList: asyncHandler(async (req, res) => {
         const products = await productService.getProductList();
         res.status(200).json(products);
-    },
+    }),
+
     // 상품 등록
-    uploadProduct: async (req, res, next) => {
+    uploadProduct: asyncHandler(async (req, res) => {
         const productData = req.body; // 요청 본문에서 상품 데이터 가져오기
         const result = await productService.uploadProduct(req, productData);
         res.status(201).json(result);
-    },
+    }),
+
     // 특정 상품 조회
-    getProduct: async (req, res, next) => {
+    getProduct: asyncHandler(async (req, res) => {
         const { productId } = req.params; // URL에서 상품 ID 추출
         const product = await productService.getProduct(productId);
         res.status(200).json(product); // 성공 응답
-    },
+    }),
+
     // 상품 수정
-    updateProduct: async (req, res, next) => {
+    updateProduct: asyncHandler(async (req, res) => {
         const { productId, updateData } = req.body; // 요청 본문에서 상품 ID와 수정할 데이터 가져오기
         const result = await productService.updateProduct(productId, updateData);
         res.status(204).json(result); // 상품 수정 성공
-    },
+    }),
+
     // 상품 삭제
-    deleteProduct: async (req, res, next) => {
+    deleteProduct: asyncHandler(async (req, res) => {
         const { productId } = req.body; // 요청 본문에서 상품 ID 가져오기
         const result = await productService.deleteProduct(productId);
         res.status(204).json(result);
-    },
+    }),
 };
 
 export default productController;
