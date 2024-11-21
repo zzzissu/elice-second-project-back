6831d9856e4cc543ba54d3091ef78193b2ee1773
최준영 <happy77523@naver.com>
2024-11-21 18:21:33 +0900

    fix: 에러대신 결과창 반환

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -5,8 +5,19 @@ import { NotFoundError } from '../class/errorClass.js';
 const OrderService = {
     // 결제 페이지
     createOrder: async (order) => {
-        const { buyerId, buyerName, postalCode, basicAdd, detailAdd, phone, 
-                productId, productName, productImg, productPrice, request } = order;
+        const {
+            buyerId,
+            buyerName,
+            postalCode,
+            basicAdd,
+            detailAdd,
+            phone,
+            productId,
+            productName,
+            productImg,
+            productPrice,
+            request,
+        } = order;
 
         // 결제 페이지
         const newOrder = new Order({
@@ -25,7 +36,6 @@ const OrderService = {
 
         await newOrder.save();
         return { message: '상품이 성공적으로 결제되었습니다.', order: newOrder };
-
     },
 
     // 구매내역 조회
@@ -35,7 +45,10 @@ const OrderService = {
             .populate('seller.userId', 'nickname'); // 판매자 정보
 
         if (orders.length === 0) {
-            throw new NotFoundError('구매한 주문이 없습니다.');
+            return {
+                orders: [],
+                message: '구매한 주문이 없습니다.',
+            };
         }
 
         return orders; // 주문 목록 반환
