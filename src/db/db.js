3bf2e1d9d1dc7b28302e3e9599e8756f16bf8e98
최준영 <happy77523@naver.com>
2024-11-23 01:26:22 +0900

    feat:aws S3사용 코드 작성

--- src/db/db.js
+++ src/db/db.js
@@ -2,10 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('MongoDB connected');
     } catch (error) {
         console.error('MongoDB connection error:', error);
